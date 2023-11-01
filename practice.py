f9da5b1e94994e8995bf1d624c9406d2286b3924
Bea Parcutela <beaparcutela@gmail.com>
2023-10-31 21:30:32 -0700

    drew a picture of my cat

--- practice.py
+++ practice.py
@@ -1,5 +1,12 @@
 print('hello this is Bea ')
 
-s = 'hello world'
+s = 'hello world, this is my cat.'
 
 print (s)
+
+#milestone = Mamon
+
+print(" ^  ^ ")
+print ( "(o x o)")
+#print ( "( x )")
+print (" ( w ) ")
